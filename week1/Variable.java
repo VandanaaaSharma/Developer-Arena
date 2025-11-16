973d7d2131b9bc90cddc908b0f90624386ea9d81
VandanaaaSharma <vandanasharmajewar007@gmail.com>
2025-11-16 20:21:14 +0530

    Done

---
+++ week1/Variable.java
@@ -0,0 +1,16 @@
+public class Variable {
+    public static void main(String[] args) {
+        
+        int age = 18;
+        double price = 99.99;
+        boolean isStudent = true;
+        String name = "Alice";
+
+        System.out.println(age);
+        System.out.println(price);
+        System.out.println(isStudent);
+        System.out.println(name);
+    }
+
+
+}
